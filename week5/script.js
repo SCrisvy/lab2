3f279347c41ee2d1df9abdaef317a17ad8c9ffda
shane Ricafort <sgricafort@student.apc.edu.ph>
2023-01-24 21:28:55 +0800

    fix.3

--- week5/script.js
+++ week5/script.js
@@ -25,11 +25,5 @@ for(let i = 0; i < allBtns.length; i++){
     });
 }
 
-function mOver(obj) {
-  obj.innerHTML = "What stresses me out: "
-}
 
-function mOut(obj) {
-  obj.innerHTML = "Course: "
-}
 }
