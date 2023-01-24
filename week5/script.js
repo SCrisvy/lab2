c632e28cf4bbc9fb7924d8bf0fe434a091328a60
shane Ricafort <sgricafort@student.apc.edu.ph>
2023-01-24 22:13:24 +0800

    Lesson 22.10: Event Listener

--- week5/script.js
+++ week5/script.js
@@ -25,5 +25,15 @@ for(let i = 0; i < allBtns.length; i++){
     });
 }
 
+function showDate() {
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
+  document.getElementById("date-display").innerHTML = dateTime;
+}
+document.getElementById("date-button").addEventListener("click", showDate);
+var date = new Date().toDateString();
+var time = new Date().toTimeString();
 
 }
