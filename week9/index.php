6743b18f89737283e0533329630874e02c99605e
shane Ricafort <sgricafort@student.apc.edu.ph>
2023-02-03 22:20:49 +0800

    5

--- week9/index.php
+++ week9/index.php
@@ -193,21 +193,21 @@
 				<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 				  Name: <input type="text" name="name" value="<?php echo $name;?>">
 				  <span class="error">* <?php echo $nameErr;?></span>
-				  <br><br>
+				  <br>
 				  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
 				  <span class="error">* <?php echo $emailErr;?></span>
-				  <br><br>
+				  <br>
 				  Website: <input type="text" name="website" value="<?php echo $website;?>">
 				  <span class="error"><?php echo $websiteErr;?></span>
-				  <br><br>
+				  <br>
 				  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-				  <br><br>
+				  <br>
 				  Gender:
 				  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
 				  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
 				  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
 				  <span class="error">* <?php echo $genderErr;?></span>
-				  <br><br>
+				  <br>
 				  <input type="submit" name="submit" value="Submit">  
 				</form>
 
